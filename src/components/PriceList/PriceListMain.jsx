e0661b6935cae60680cbc0ecae31ea7347af2b27
Asel Alimbaeva <asel_19.86@mail.ru>
2022-01-03 23:15:53 +0300

    finish price list

--- src/components/PriceList/PriceListMain.jsx
+++ src/components/PriceList/PriceListMain.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import Acardion from '../../page/Sections/Acardion/Acardion'
+import Acardion from '../../page/Sections/Acardion/Acardion';
+import ExpertAdvice from '../../page/Sections/ExpertAdvice/ExpertAdvice';
+import Slider from '../../page/Sections/Slider_Feedback/Slider_Feedback';
+import './priceListMain.scss';
 
 function PriceListMain({ children }) {
     return (
-        <div id='PriceListMain'>
-            <h1>PriceListMain</h1>
+        <section id='PriceListMain' className='priceListMain'>
+            <h2 className='priceListMain__title'>Стоимость услуг</h2>
             <Acardion />
+            <div className="priceListMain__blockLine"></div>
+            <div className="container">
+                <p className='priceListMain__text'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Placeat necessitatibus labore earum quae eius perferendis ipsum,
+                    a nesciunt ab facere debitis corporis porro consequuntur delectus consectetur
+                    laborum commodi id mollitia amet ratione suscipit adipisci est tempora! Accusamus
+                    Sapiente inventore odit voluptates, unde impedit soluta.
+                </p>
+            </div>
+            <Slider />
+            <ExpertAdvice />
             <div >{children}</div>
-        </div>
+        </section>
     )
 }
 
